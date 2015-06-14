0d2f9ce6c2e995415507c2fdc6db199e62c893ea
罗雪 <974383157@qq.com>
2015-06-14 15:54:04 +0800

    code commite

---
+++ Excellent/WebContent/pages/common/common.js
@@ -0,0 +1,27 @@
+$(function(){
+	//common.getBanner();
+});
+
+var common = {};
+
+(function(page){
+	page.getBanner = function(i){
+		$.ajax({
+			url:'bannerMngr.html',
+			type:"get",
+			dataType:'html',
+			success:function(data){
+				$("#banner").html(data);
+				page.serActive(i);
+			}
+		});
+	}
+	
+	page.serActive = function(index){
+		$("#barLink li").each(function(i){
+			if(i == index){
+				$(this).addClass('active');
+			}
+		});
+	};
+})(common);
