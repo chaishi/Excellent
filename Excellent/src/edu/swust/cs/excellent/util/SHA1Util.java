c3b938e01af1a68f3850ae56b02ac863fe25ca62
mlt940109 <591744873@qq.com>
2015-07-11 14:11:01 +0800

    。。

---
+++ Excellent/src/edu/swust/cs/excellent/util/SHA1Util.java
@@ -0,0 +1,14 @@
+package edu.swust.cs.excellent.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+
+public class SHA1Util {
+	
+	public final static String SHA1(String s){
+		
+		String string=DigestUtils.shaHex(s);
+		return string;
+		
+	}
+}
