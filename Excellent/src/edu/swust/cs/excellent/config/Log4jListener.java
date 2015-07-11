93d4d2e904078a2412e1558aee986033a2af4804
mlt940109 <591744873@qq.com>
2015-07-11 15:54:48 +0800

    更改日志位置对系统适应性

--- Excellent/src/edu/swust/cs/excellent/config/Log4jListener.java
+++ Excellent/src/edu/swust/cs/excellent/config/Log4jListener.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletContextListener;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 import com.jfinal.core.Const;
+import com.jfinal.kit.PathKit;
 
 public class Log4jListener implements ServletContextListener {
 	 public static final String log4jDirKey=Constant.LOG4JDIRKEY;
@@ -17,7 +18,7 @@ public class Log4jListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 	  // String log4jDir = Constant.LOG4JDIR;
-	   String log4jDir = arg0.getServletContext().getRealPath("/")+Constant.LOG4JDIR;
+	   String log4jDir = PathKit.getRootClassPath()+Constant.LOG4JDIR;
 	 //  System.out.println("log4jdir:"+log4jDir);
 	   System.setProperty(log4jDirKey,log4jDir );
 	}
